dd7a975e7602d659bb514810cba08bdd050c6ebc
Alex Lalejini <amlalejini@gmail.com>
2019-03-08 16:23:19 -0500

    add simple phylogeny converter command line test

--- tests/test_phylogeny_converter.py
+++ tests/test_phylogeny_converter.py
@@ -1,6 +1,10 @@
+import subprocess
 
-def test_placeholder():
-    pass
+def test_mabeData2StdPhylogeny_CmdLine():
+    cmd = "python3 mabeData2stdPhylogeny.py"
+    path = "example_data/example_mabe_output_asexual/snapshot_data/"
+    subprocess.run(f"{cmd} -path {path}", shell=True)
+    subprocess.run("rm lineage.csv", shell=True)
 
 if __name__ == "__main__":
-    pass
+    test_mabeData2StdPhylogeny_CmdLine()
