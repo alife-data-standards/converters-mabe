7f2337650273be94a3aaba1ee12578129ad1b67c
Alex Lalejini <amlalejini@gmail.com>
2019-03-08 16:51:21 -0500

    update tests

--- tests/test_phylogeny_converter.py
+++ tests/test_phylogeny_converter.py
@@ -2,10 +2,15 @@ import subprocess
 import mabe_converters as mc
 
 def test_mabeData2StdPhylogeny_CmdLine():
-    cmd = "python3 mabeData2stdPhylogeny.py"
+    cmd = "python3 mabe_converters/phylogeny.py"
     path = "example_data/example_mabe_output_asexual/snapshot_data/"
     subprocess.run(f"{cmd} -path {path}", shell=True)
     subprocess.run("rm lineage.csv", shell=True)
 
+def test_ConverterFunction():
+    mc.ConvertMABESnapshotsToStdPhylogeny("example_data/example_mabe_output_asexual/snapshot_data/")
+    subprocess.run("rm lineage.csv", shell=True)
+
 if __name__ == "__main__":
     test_mabeData2StdPhylogeny_CmdLine()
+    test_ConverterFunction()
